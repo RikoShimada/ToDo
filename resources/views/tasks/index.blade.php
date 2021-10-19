30f208aea71f0906417561d8ec6a70d7d73e44d7
RikoShimada <nashicoral@gmail.com>
2021-10-19 18:54:12 +0900

    edit 211019

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,21 +8,24 @@
   <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
-<header>
+<!-- <header>
   <nav class="my-navbar">
     <a class="my-navbar-brand" href="/">ToDo App</a>
   </nav>
-</header>
+</header> -->
 <main>
+@extends('layout')
+
+@section('content')
   <div class="container">
     <div class="row">
       <div class="col col-md-4">
         <nav class="panel panel-default">
           <div class="panel-heading">フォルダ</div>
           <div class="panel-body">
-            <a href="#" class="btn btn-default btn-block">
-              フォルダを追加する
-            </a>
+          <a href="{{ route('folders.create') }}" class="btn btn-default btn-block">
+            フォルダを追加する
+          </a>
           </div>
           <div class="list-group">
             @foreach($folders as $folder)
@@ -36,9 +39,48 @@
       </div>
       <div class="column col-md-8">
         <!-- ここにタスクが表示される -->
-      </div>
+        
+        <div class="panel panel-default">
+            <div class="panel-heading">タスク</div>
+            <div class="panel-body">
+                <div class="text-right">
+                <a href="{{ route('tasks.create', ['id' => $current_folder_id]) }}" class="btn btn-default btn-block">
+                    タスクを追加する
+                </a>
+                </div>
+            </div>
+            <table class="table">
+                <thead>
+                <tr>
+                <th>タイトル</th>
+                <th>状態</th>
+                <th>期限</th>
+                <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($tasks as $task)
+                    <tr>
+                    <td>{{ $task->title }}</td>
+                    <td>
+                        <span class="label {{ $task->status_class }}">{{ $task->status_label }}</span>
+                    </td>
+                    <td>{{ $task->formatted_due_date }}</td>
+                    <td>
+                    <a href="{{ route('tasks.edit', ['id' => $task->folder_id, 'task_id' => $task->id]) }}">
+                        編集
+                    </a>
+                    </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+            </div>
+        </div>
+
     </div>
   </div>
+@endsection
 </main>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,22 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', 'PlayersController@index');
 // Route::get('/index', 'PlayersController@index');
 
-Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+Route::group(['middleware' => 'auth'], function() {
+
+    Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+
+    Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+    Route::post('/folders/create', 'FolderController@create');
+
+    Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+    Route::post('/folders/{id}/tasks/create', 'TaskController@create');
+
+    Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+    Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+
+    Route::get('/', 'HomeController@index')->name('home');
+
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+
+Auth::routes();
